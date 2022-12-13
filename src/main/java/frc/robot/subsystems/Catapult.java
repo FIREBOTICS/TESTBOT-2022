12acb2874cd5c0ca7912965ea711562010aa478b
Narnillian <ner.baby@gmail.com>
2022-12-13 14:56:35 -0800

    Apply PneumaticsModuleType

--- src/main/java/frc/robot/subsystems/Catapult.java
+++ src/main/java/frc/robot/subsystems/Catapult.java
@@ -1,12 +1,13 @@
 package frc.robot.subsystems;
 
+import edu.wpi.first.wpilibj.PneumaticsModuleType;
 import edu.wpi.first.wpilibj.Solenoid;
 import edu.wpi.first.wpilibj2.command.SubsystemBase;
 import frc.robot.Constants;
 
 
 public class Catapult extends SubsystemBase {
-    private final Solenoid m_Solenoid = new Solenoid(null, Constants.solenoidChannel);
+    private final Solenoid m_Solenoid = new Solenoid(PneumaticsModuleType.REVPH, Constants.solenoidChannel);
 
     public void retract() {
         m_Solenoid.set(Constants.solenoidRetract);
